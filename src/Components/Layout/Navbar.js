a4232f19fcd10415a2405c27adbb3472af9b9336
BatyaFeldman <batyashaps1@gmail.com>
2020-08-19 12:19:02 -0500

    state, stateless functions, http requests

--- src/Components/Layout/Navbar.js
+++ src/Components/Layout/Navbar.js
@@ -1,27 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-export class Navbar extends Component {
-  static defaultProps = {
+const Navbar = ({ icon, title }) => {
+  return (
+    <div className="navbar bg-primary">
+      <h1>
+        <i className={icon} />
+        {title}
+      </h1>
+    </div>
+  );
+
+  Navbar.defaultProps = {
     title: "Github Finder",
     icon: "fab fa-github",
   };
 
-  static propTypes = {
+  Navbar.propTypes = {
     title: PropTypes.string.isRequired,
     icon: PropTypes.string.isRequired,
   };
-
-  render() {
-    return (
-      <div className="navbar bg-primary">
-        <h1>
-          <i className={this.props.icon} />
-          {this.props.title}
-        </h1>
-      </div>
-    );
-  }
-}
+};
 
 export default Navbar;
--- src/Components/users/UserItem.js
+++ src/Components/users/UserItem.js
@@ -1,36 +1,29 @@
-import React, { Component } from "./node_modules/react";
+import React from "react";
+import PropTypes from "prop-types";
 
-export class UserItem extends Component {
-  //initializing state. can be done in constructor, but this is the same thing
-  state = {
-    id: "id",
-    login: "mojombo",
-    avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-    html_url: "https://github.com/mojombo",
-  };
+//destructuring user props
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
 
-  render() {
-    //destructuring so can avoid 'this.state'
-    const { login, avatar_url, html_url } = this.state;
-
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt=""
-          className="round-img"
-          style={{ width: "60px" }}
-        />
-        <h3>{login}</h3>
-
-        <div>
-          <a href={html_url} className="btn.btn-dark.btn-sm.my-1">
-            More
-          </a>
-        </div>
+      <div>
+        <a href={html_url} className="btn.btn-dark.btn-sm.my-1">
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
 
 export default UserItem;
